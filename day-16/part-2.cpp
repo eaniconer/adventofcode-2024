b85811a78a52f31c84384ec105de00b476501275
Egor <30935034+eaniconer@users.noreply.github.com>
2024-12-16 21:07:59 +0000

    day16-2

--- day-16/part-2.cpp
+++ day-16/part-2.cpp
@@ -1,7 +1,166 @@
 #include "file_reader.hpp"
+#include "hash/pair_support.hpp"
+
+#include <unordered_map>
+#include <cassert>
+#include <unordered_set>
+#include <iostream>
+
+namespace {
+
+using Integer = size_t;
+using Field = decltype(utils::readLines(nullptr));
+
+static const char WALL = '#';
+static const char EMPTY = '.';
+static const char START = 'S';
+static const char END = 'E';
+
+}
 
 int main() {
-    auto lines = utils::readLines("input.txt");
+    static const Integer ROTATE_COST = 1000;
+    static const Integer STEP_COST = 1;
+
+    Field field = utils::readLines("input.txt");    
+    const size_t h = field.size();
+    const size_t w = field.at(0).size();
+    
+    auto convertRCLtoVertexIndex = [h, w](size_t r, size_t c, size_t level) {
+        size_t offset = (h * w) * level;
+        return (r * w + c) + offset;
+    };
+    auto convertVertexIndexToRC = [h, w](size_t vi) {
+        vi %= (h * w);
+        size_t c = vi % w;
+        size_t r = vi / w;
+        return std::make_pair(r, c);
+    };
+
+    std::unordered_map<size_t, std::unordered_map<size_t, size_t>> graph;
+    size_t edge_count = 0;
+    auto add_edge = [&graph, &edge_count](size_t u, size_t v, size_t w) {
+        graph[u][v] = w;
+        graph[v][u] = w;
+        edge_count++;
+    };
+
+    std::pair<size_t, size_t> start;
+    std::pair<size_t, size_t> end;
+
+    bool start_found = false;
+    bool end_found = false;
+    for (size_t r = 0; r < h; ++r) {
+        for (size_t c = 0; c < w; ++c) {
+            char& value = field[r][c];
+            if (value == WALL)
+                continue;
+            if (value == START) {
+                value = EMPTY;
+                start_found = true;
+                start = std::make_pair(r, c);
+            }
+            if (value == END) {
+                value = EMPTY;
+                end_found = true;
+                end = std::make_pair(r, c);
+            }
+            assert(value == EMPTY);
+            
+            auto u0 = convertRCLtoVertexIndex(r, c, 0);
+            if (r > 0 && field[r - 1][c] == EMPTY) {
+                auto v0 = convertRCLtoVertexIndex(r - 1, c, 0);
+                add_edge(u0, v0, STEP_COST);
+            }
+
+            auto u1 = convertRCLtoVertexIndex(r, c, 1);
+            if (c > 0 && field[r][c - 1] == EMPTY) {
+                auto v1 = convertRCLtoVertexIndex(r, c - 1, 1);
+                add_edge(u1, v1, STEP_COST);
+            }
+
+            add_edge(u0, u1, ROTATE_COST);
+        }
+    }
+    if (!start_found)
+        throw std::runtime_error("START not found on the field");
+    if (!end_found)
+        throw std::runtime_error("END not found on the field");
+
+    auto [sr, sc] = start;
+    auto vi = convertRCLtoVertexIndex(sr, sc, 1);
+
+    std::cout << "Graph with |V|=" << graph.size()
+        << ", |E|=" << edge_count << std::endl;
+
+    std::unordered_map<size_t, size_t> d;
+    for (const auto& [k, _] : graph)
+        d[k] = std::numeric_limits<size_t>::max();
+    d[vi] = 0;
+    std::unordered_set<size_t> visited;
+
+    // we have Graph with |V|=20176, |E|=20464
+    // the following algorithm is very-very-very slow
+    // TODO: enhance algorithm
+    while (visited.size() != graph.size()) {
+        if (visited.size() % 1000 == 0) {
+            std::cout << "visited size = " << visited.size() << std::endl;
+        }
+        size_t v = std::numeric_limits<size_t>::max();
+        size_t best_d = std::numeric_limits<size_t>::max();
+        for (const auto& [k, _] : graph) {
+            if (visited.find(k) != visited.end()) 
+                continue;
+            if (d[k] < best_d) {
+                best_d = d[k];
+                v = k;
+            }
+        }
+        assert(v != std::numeric_limits<size_t>::max());
+        visited.insert(v);
+
+        const auto& us = graph[v];
+        for (auto [u, w] : us) {
+            if (visited.find(u) != visited.end())
+                continue;
+            if (d[v] + w < d[u]) {
+                d[u] = d[v] + w;
+            }
+        }
+    }
+
+    auto [er, rc] = end;
+    auto best_dist = std::min(d[convertRCLtoVertexIndex(er, rc, 0)], d[convertRCLtoVertexIndex(er, rc, 1)]);
+    std::cout << " DIST: " 
+        << best_dist
+        << std::endl;
+
+    auto ei0 = convertRCLtoVertexIndex(er, rc, 0);
+    auto ei1 = convertRCLtoVertexIndex(er, rc, 1);
+
+    std::unordered_set<std::pair<size_t, size_t>> positions;
+    positions.insert(end);
+
+    std::unordered_set<size_t> to_see;
+    if (d[ei0] == best_dist)
+        to_see.insert(ei0);
+    if (d[ei1] == best_dist)
+        to_see.insert(ei1);
+   
+    std::unordered_set<size_t> new_to_see;
+    while (!to_see.empty()) {
+        new_to_see.clear();
+        for (auto v : to_see) {
+            for (auto [u, w] : graph[v]) {
+                if (d[u] + w == d[v]) {
+                    new_to_see.insert(u);
+                    positions.insert(convertVertexIndexToRC(u));
+                }
+            }
+        }
+        std::swap(new_to_see, to_see);
+    }
+    std::cout << "Tiles count: " << positions.size() << std::endl;
 
     return 0;
 }
